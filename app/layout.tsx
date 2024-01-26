88db6e6ae8b78f36304ef8b5f8dce9649f06c6f8
rof1yev <rofiyevdilshod@gmail.com>
2024-01-26 17:01:20 +0500

    nextjs toploader

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Figtree } from "next/font/google";
 // Global Styles
 import "./globals.css";
 
+import NextTopLoader from "nextjs-toploader";
 import Sidebar from "@/components/Sidebar";
 import SupabaseProvider from "@/providers/SupabaseProvider";
 import UserProvider from "@/providers/UserProvider";
@@ -35,6 +36,21 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body className={figtree.className}>
+        <NextTopLoader
+          color="#22c55e"
+          initialPosition={0.08}
+          crawlSpeed={200}
+          height={3}
+          crawl={true}
+          showSpinner={true}
+          easing="ease"
+          speed={200}
+          shadow="0 0 10px #22c55e,0 0 5px #22c55e"
+          template='<div class="bar" role="bar"><div class="peg"></div></div> 
+  <div class="spinner" role="spinner"><div class="spinner-icon"></div></div>'
+          zIndex={1600}
+          showAtBottom={false}
+        />
         <ToasterProvider />
         <SupabaseProvider>
           <UserProvider>
